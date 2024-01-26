0d5da32ea75e9abd780b7fa200192f4334f1fc04
UncannyFish <lomtvador@gmail.com>
2024-01-26 06:32:40 +0300

    Increase MAX_POLYS and MAX_POLYVERTS

--- src/renderer/tr_local.h
+++ src/renderer/tr_local.h
@@ -1855,8 +1855,8 @@ typedef enum {
 // these are sort of arbitrary limits.
 // the limits apply to the sum of all scenes in a frame --
 // the main view, all the 3D icons, etc
-#define	MAX_POLYS		600
-#define	MAX_POLYVERTS	3000
+#define	MAX_POLYS		32000
+#define	MAX_POLYVERTS	128000
 
 // all of the information needed by the back end must be
 // contained in a backEndData_t.  This entire structure is
