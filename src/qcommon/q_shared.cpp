92bde1e50102a873333bdc78836fa644ba76c175
UncannyFish <lomtvador@gmail.com>
2024-04-19 17:33:51 +0300

    Fix crash in COM_ParseExt and CommaParse

--- src/qcommon/q_shared.cpp
+++ src/qcommon/q_shared.cpp
@@ -381,7 +381,7 @@ char *COM_ParseExt(const char **data_p, qboolean allowLineBreaks) {
 				*data_p = (const char *)data;
 				return com_token;
 			}
-			if (len < MAX_TOKEN_CHARS) {
+			if (len < MAX_TOKEN_CHARS - 1) {
 				com_token[len] = c;
 				len++;
 			}
@@ -390,7 +390,7 @@ char *COM_ParseExt(const char **data_p, qboolean allowLineBreaks) {
 
 	// parse a regular word
 	do {
-		if (len < MAX_TOKEN_CHARS) {
+		if (len < MAX_TOKEN_CHARS - 1) {
 			com_token[len] = c;
 			len++;
 		}
@@ -400,10 +400,6 @@ char *COM_ParseExt(const char **data_p, qboolean allowLineBreaks) {
 			com_lines++;
 	} while (c>32);
 
-	if (len == MAX_TOKEN_CHARS) {
-		//		Com_Printf ("Token exceeded %i chars, discarded.\n", MAX_TOKEN_CHARS);
-		len = 0;
-	}
 	com_token[len] = 0;
 
 	*data_p = (const char *)data;
